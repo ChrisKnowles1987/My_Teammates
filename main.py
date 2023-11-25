d8e2f8666e8010de0ed91f5501a28e50986e736a
ChrisKMBP <chrisknowles1987@me.com>
2023-11-25 07:01:08 +0000

    add % formating

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+'''
+This part of the project is complete, dom reccomends writing this script in 
+javascript and using express js
+
+TODO
+look into expressJS
+write hello world with extressJS and node
+make repo and call it 'TeamDiff'and push hello world
+make html css for UI
+'''
+
 
 import requests
 from dotenv import load_dotenv
@@ -25,13 +36,11 @@ api_LEAGUE_v4= 'https://euw1.api.riotgames.com/lol/league/v4/entries/by-summoner
 def get_summoner_info():
     summoner_info_url = f'{api_SUMMONER_v4}{name}'
     summoner_info = requests.get(summoner_info_url, headers=headers).json()
-    # print(f'summoner info: {summoner_info}')
     return summoner_info
 
 def get_matches(summoner_info):
     summoner_matches_url = f"{api_MATCHES_v5}{summoner_info['puuid']}/ids?type=ranked&start=0&count=20"
     summoner_matches = requests.get(summoner_matches_url, headers = headers).json()
-    #print(f'summoner_matches: {summoner_matches}')
     return summoner_matches
 
 def get_participants(summoner_matches):
@@ -43,8 +52,6 @@ def get_participants(summoner_matches):
         summoner_names.append(get_teammates_with_matching_id(response['info']['participants'],teamId))
     return summoner_names
     
-        
- 
 def get_team_id(participants):
      for participant in participants:
         if participant['summonerName'] == name:
@@ -57,47 +64,35 @@ def get_teammates_with_matching_id(participants, teamId):
             team_array.append(participant['summonerId'])
     return team_array
 
-def get_winrate(participants, queue_type): # 'RANKED_SOLO_5X5' or 'RANKED_FLEX_SR'
-    winrates =[]
-    queue_type = queue_type
+def get_winrate(participants, queue_type): # 'RANKED_SOLO_5x5' or 'RANKED_FLEX_SR'
+    winrates ={}
     for match in participants:
         for player in match:
             req_url=f'{api_LEAGUE_v4}{player}'
             response = requests.get(req_url,headers=headers).json()
             for item in response:
-                if item.get('queueType') == queue_type:                    
+                if item.get('queueType') == queue_type: 
+                    summonerName = item.get('summonerName')                 
                     wins = item.get('wins',0)
                     losses = item.get('losses', 0)
                     total_games = wins + losses
                     if total_games > 0:
-                        winrate = round((wins / total_games),2)
-                        winrates.append(winrate)
+                        winrate = f'{round(((wins / total_games)*100),1)} %'
+                        winrates[summonerName] = winrate
                     else:
                         winrates.append(0)  # Append 0 if no games played
-                    #losses.append(item.get('losses',0))
                 else:
                     continue        
                         
-
-    print(f'winrates for {queue_type}: {winrates}')
-    
-            
-            
-            
-            
+    return winrates
     
 def main():
     print('running...')
     summoner_info = get_summoner_info()
     summoner_matches = get_matches(summoner_info)   
     participants = get_participants(summoner_matches)
-    get_winrate(participants,queue_type='RANKED_SOLO_5X5')# 'RANKED_SOLO_5X5' or 'RANKED_FLEX_SR'
-    #get_winrate(participants,'RANKED_FLEX_SR')
-    
-    
-    
-    
-    
+    winrates = get_winrate(participants,'RANKED_SOLO_5x5' )# 'RANKED_SOLO_5x5' or 'RANKED_FLEX_SR'
+    print(winrates)
     
 if __name__ == '__main__':
     main()
